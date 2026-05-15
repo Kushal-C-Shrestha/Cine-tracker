74974ee9ac84af58e0e646ad7e23e7c9d306ca7e
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-05-15 15:33:08 +0545

    Created navbar component, main layout and updated app route using protected route for home.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,6 +3,9 @@ import AuthLayout from '@/layouts/AuthLayout'
 import Register from '@/features/auth/pages/Register.tsx'
 import Login from '@/features/auth/pages/Login.tsx'
 import PublicRoute from './PublicRoute'
+import ProtectedRoute from './ProtectedRoute'
+import Home from '@/features/movies/pages/Home'
+import MainLayout from '@/layouts/MainLayout'
 
 export default function AppRoutes() {
     return (
@@ -12,6 +15,13 @@ export default function AppRoutes() {
                     <Route element={<AuthLayout />} >
                         <Route path="/register" element={<Register />} />
                         <Route path="/login" element={<Login />} />
+                    </Route>
+                </Route>
+            </Routes>
+            <Routes>
+                <Route element={<ProtectedRoute />}>
+                    <Route element={<MainLayout />} >
+                        <Route path="/" element={<Home />} />
                     </Route>
                 </Route>
             </Routes>
