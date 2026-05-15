23d6bab84c8507ce4910e58a2aa9422c1055e48b
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-05-15 16:53:13 +0545

    Implemented the movie grid in home page to display movies.

--- src/features/movies/pages/Home/index.tsx
+++ src/features/movies/pages/Home/index.tsx
@@ -1,6 +1,12 @@
+import MovieGrid from '@/features/movies/components/MovieGrid'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex flex-1 min-h-0 overflow-hidden">
+      <div className="flex-1 min-w-0 overflow-y-auto scroll-smooth scrollbar-none flex flex-col px-8 py-6">
+        <MovieGrid />
+      </div>
+    </div>
   )
 }
 
