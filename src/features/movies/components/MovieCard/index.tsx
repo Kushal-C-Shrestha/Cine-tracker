12edb6d83cf59d870fb5cbf6fe05135721239b06
Kushal Chandra Shrestha <kushal.c.shrestha@gmail.com>
2026-05-15 16:52:07 +0545

    Created card and grid components for displaying fetched movies.

---
+++ src/features/movies/components/MovieCard/index.tsx
@@ -0,0 +1,31 @@
+type MovieCardProps = {
+    movie?: {
+        id: number
+        title: string
+        rating: number
+        genre: string[]
+        posterUrl?: string
+        isWatchlisted?: boolean
+    }
+}
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+    return (
+        <div className="flex flex-col w-full cursor-pointer">
+            <div className="rounded-lg overflow-hidden">
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${movie?.posterUrl}`}
+                    alt={movie?.title}
+                    className="w-full h-64 object-cover object-top"
+                />
+            </div>
+            <div className="flex justify-between w-full mt-2 gap-2">
+                <h2 className="text-sm text-white flex-1 truncate">{movie?.title}</h2>
+                <p className="text-sm text-white">{movie?.rating.toFixed(1)}/10</p>
+            </div>
+            <p className="text-xs text-zinc-400 mt-1">{movie?.genre.slice(0, 3).join(' · ')}</p>
+        </div>
+    )
+}
+
+export default MovieCard
---
+++ src/features/movies/components/MovieGrid/index.tsx
@@ -0,0 +1,46 @@
+import MovieCard from '@/features/movies/components/MovieCard'
+import { useGetGenresQuery, useGetMoviesQuery } from '@/features/movies/services/tmdbMoviesApi'
+import Loader from '@/components/atoms/Loader'
+
+const MovieGrid = () => {
+  const { data: genres = {} } = useGetGenresQuery()
+  const { data: movies = [], isLoading } = useGetMoviesQuery()
+
+  if (isLoading) {
+    return (
+      <div className="flex-1 flex items-center justify-center gap-2">
+        <Loader className="text-gray-500" />
+        <p className="text-gray-500">Loading movies...</p>
+      </div>
+    )
+  }
+
+  if (movies.length === 0) {
+    return (
+      <div className="flex-1 flex items-center justify-center">
+        <p className="text-gray-500">No movies found</p>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="grid gap-4 md:gap-6 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
+        {movies.map((movie: any) => (
+          <MovieCard
+            key={movie.id}
+            movie={{
+              id: movie.id,
+              title: movie.title,
+              rating: movie.vote_average,
+              genre: movie.genre_ids.map((id: number) => genres[id] || 'Unknown'),
+              posterUrl: movie.poster_path,
+            }}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default MovieGrid
